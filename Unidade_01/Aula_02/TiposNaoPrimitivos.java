dd89d042549268fdbbab6c997a44d5a03fa62ef9
biizerrah <biizerrah@outlook.com>
2024-09-04 17:41:28 -0300

    Update about non premitive data types in java. String, Enum and Arrays

--- Unidade_01/Aula_02/TiposNaoPrimitivos.java
+++ Unidade_01/Aula_02/TiposNaoPrimitivos.java
@@ -1,3 +1,4 @@
+
 public class TiposNaoPrimitivos {
     public static void main(String[] args) {
         
@@ -7,7 +8,34 @@ public class TiposNaoPrimitivos {
         Quando temos um número que não será modificado no código é recomendado atribui - lo a uma constante. 
         Uma constante é identificada pela palavra reservada 'final'. Assim o valor que for atribuido a uma consoante não pode ser alterada, apenas consultada.
         */
+        System.out.println(PI);
 
-        System.out.print(PI);
+        //Cadeia de caracteres 
+        String nome = "Tatiana Bezerra";
+        //A String é um tipo não primitivo e junta vários caracteres, inclusive os espaços que formam palavras e frases. A atribuição deve ser feita por aspas duplas [""]
+        System.out.println(nome);
+
+        //ENUM - é um conjunto de palavras que serão usadas frequentemente e que não tem  seu valor alterado.alterado.
+        enum diaSemana {
+
+            DOMINGO,SEGUNDA_FEIRA, 
+            TERÇA_FEIRA,QUARTA,QUINTA,
+            SEXTA,SÁBADO
+        } 
+
+        //Não aceita aspas e nem espaços e seus valores são acessados da seguinte forma...
+        diaSemana segunda = diaSemana.SEGUNDA_FEIRA;
+        diaSemana domingo = diaSemana.DOMINGO;
+        System.out.println(segunda);
+        System.out.println(domingo);
+
+        //Arrays são listas de dados de um tipo definido. Diferente de variaveis os arrays conseguem armazenar mais de um dado por vez. 
+        int[] numeros = {1,2,3}; //Aqui definimos uma lista do tipo inteiro. Então ela vai guardar vários números iteiros de uma vez só.
+
+        System.out.print(numeros[0]); //Os valores são acessados por meio de indices, que começam sempre em 0.Então para acessar o 1º valor usamos numeros[0].
+
+
+
+
     }
 }
