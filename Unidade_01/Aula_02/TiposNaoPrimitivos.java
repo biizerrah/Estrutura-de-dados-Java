f2ee68ceb4229630ebfb6a13df4e56c1e1c2f45a
biizerrah <biizerrah@outlook.com>
2024-09-04 16:51:00 -0300

    Non primitive data types in Java and an exemple of a const

---
+++ Unidade_01/Aula_02/TiposNaoPrimitivos.java
@@ -0,0 +1,13 @@
+public class TiposNaoPrimitivos {
+    public static void main(String[] args) {
+        
+        //Exemplo de constate em Java
+        final double PI = 3.145128;
+         /*
+        Quando temos um número que não será modificado no código é recomendado atribui - lo a uma constante. 
+        Uma constante é identificada pela palavra reservada 'final'. Assim o valor que for atribuido a uma consoante não pode ser alterada, apenas consultada.
+        */
+
+        System.out.print(PI);
+    }
+}
