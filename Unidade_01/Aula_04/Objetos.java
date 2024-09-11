0194643c6597824010a29cb178de0854b6f8c1a0
biizerrah <biizerrah@outlook.com>
2024-09-11 16:48:37 -0300

    static da classe estava faltando e o programa não rodou. Pelo visto o static é muito importante

--- Unidade_01/Aula_04/Objetos.java
+++ Unidade_01/Aula_04/Objetos.java
@@ -1,9 +1,9 @@
 public class Objetos{
 
     //Atributos
-    private String name;
-    private int age;
-    private float weight;
+    private final String name;
+    private final int age;
+    private final float weight;
 
     //Construtor
     public Objetos(String name,int age,float weight){
@@ -14,18 +14,21 @@ public class Objetos{
 
     //Metodos
     public void mostrarInfo(){
-
+        System.out.println("===============");
         System.out.println("Nome: " + name);
         System.out.println("Idade: " + age);
         System.out.println("Peso: " + weight);
+        System.out.println("===============");
     }
-    public void main(String[] args){
+    public static void main(String[] args){
 
         //Declarando um novo objeto
         Objetos pessoa1 = new Objetos("Tatiana", 30, 94.5f);
         Objetos pessoa2 = new Objetos("Ticiana", 32, 87.5f);
+        Objetos pessoa3 = new Objetos("Marize", 58, 52.0f);
 
         pessoa1.mostrarInfo();
         pessoa2.mostrarInfo();
+        pessoa3.mostrarInfo();
     }
 }
