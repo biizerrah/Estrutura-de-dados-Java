f4185175ca909d705a280a6eaa32ca3b1884b7a4
biizerrah <biizerrah@outlook.com>
2024-09-25 16:11:01 -0300

    Entendendo o funcionamento dos getter e setter, this e constructor em uma classe java.

---
+++ Unidade_01/Aula_05/Pessoa.java
@@ -0,0 +1,63 @@
+public class Pessoa {
+
+    //Atributos
+    private String name;
+    private int age;
+    private float weight;
+
+    //Construtor
+    /* O construtor é a estrutura básica usada para a criação de uma nova instancia ou objeto. 
+    Aqui podemos definir que informações pessoa terá ao ser criada. É recomendado usar o construtor, visto que se não fizermos
+    o próprio java criar ele com dados padrão(0 para números e null para strings) então para evitar comportamentos inesperados no código
+    é uma boa prática usar o construtor */
+
+    public Pessoa(String name,int age,float weight){
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+    }
+
+    //Metodos
+    public void mostrarInfo(){
+        System.out.println("===============");
+        System.out.println("Nome: " + getName());
+        System.out.println("Idade: " + getAge());
+        System.out.println("Peso: " + weight);
+        System.out.println("===============");
+    }
+
+    public void niver(){
+        this.age += 1;
+        // ou this.age = this.age + 1;
+    };
+    /* This faz referencia a variavel especifica da classe. Ele diferencia os atributos da classe das variaveis dos metodos.  */
+
+    public String getName(){
+        return this.name;
+    };
+    
+    public void setName(String name){
+        this.name = name + '*';
+    };
+
+    public int getAge(){
+        return this.age;
+    };
+
+    public void setAge(int age){
+        this.age = age;
+    };
+
+    public static void main(String[] args){
+
+        Pessoa pessoa = new Pessoa("Tatiana", 30, 94.0f);
+        //pessoa.mostrarInfo();
+        //pessoa.niver();
+        pessoa.mostrarInfo();
+        pessoa.setAge(36);
+        pessoa.mostrarInfo();
+        pessoa.setName("Ticiana");
+        pessoa.mostrarInfo();
+
+    }
+};
