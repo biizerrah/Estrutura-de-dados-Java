b65924f27374b38fd98e1cbc51773e2e38919903
biizerrah <biizerrah@outlook.com>
2024-09-25 18:46:06 -0300

    Revendo laços. Começando pelo laço for.

---
+++ Unidade_01/Aula_06/Lacos.java
@@ -0,0 +1,9 @@
+public class Lacos{
+    public static void main(String[] args){
+
+        //Laço for: Usado para iterar sobre listas onde sabemos o tamanho especifico ou quando sabemos exatamente a quantidade de vezes que queremos a repetição.
+        for(int i = 1; i<=10; i++){
+            System.out.println(i);
+        };
+    };
+};
